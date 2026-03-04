69921f40f0ec8aa0a6f65ad60f15730b6bc2c855
ariffaysal <ariffaysal001@gmai.com>
2026-03-04 11:24:32 +0600

    rf

--- dist/main.js
+++ dist/main.js
@@ -5,16 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common");
 const morgan_1 = __importDefault(require("morgan"));
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use((0, morgan_1.default)('dev'));
     app.enableCors({
-        origin: true,
+        origin: 'http://localhost:3000',
         credentials: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         allowedHeaders: 'Content-Type,Authorization',
     });
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
     await app.listen(process.env.PORT || 3000);
     console.log(`🚀 Backend running on port ${process.env.PORT || 3000}`);
 }
